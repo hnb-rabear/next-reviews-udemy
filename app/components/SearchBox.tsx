755eb6e42f9cdf4dca1292707ce55818c0a35e7d
RadBear <nbhung71711@gmail.com>
2023-11-09 22:06:35 +0700

    Combobox Events

--- app/components/SearchBox.tsx
+++ app/components/SearchBox.tsx
@@ -2,6 +2,7 @@
 import { Combobox } from "@headlessui/react";
 import { useEffect, useState } from "react";
 import { useIsClient } from "@/lib/hook";
+import { useRouter } from "next/navigation";
 
 const reviews = [
 	{ slug: "subnautica-23", title: "Subnautica Update 3" },
@@ -17,24 +18,34 @@ const reviews = [
 ];
 
 export const SearchBox = () => {
+	const router = useRouter();
 	const isClient = useIsClient();
+	const [query, setQuery] = useState("");
 
 	// do not render on the server
 	if (!isClient) {
 		return null;
 	}
 
+	const handleChange = (review) => {
+		router.push("/reviews/" + review.slug);
+	};
+
+	const filtered = reviews.filter((review) => review.title.includes(query));
+
 	// render on the client
 	return (
 		<div className="relative w-48">
-			<Combobox>
+			<Combobox onChange={handleChange}>
 				<Combobox.Input
 					placeholder="Search..."
 					className="border px-2 py-1 rounded"
+					onChange={(event) => setQuery(event.target.value)}
+					value={query}
 				/>
 				<Combobox.Options className="absolute bg-white">
-					{reviews.map((review) => (
-						<Combobox.Option key={review.slug} value={review.slug}>
+					{filtered.map((review) => (
+						<Combobox.Option key={review.slug} value={review}>
 							{({ active }) => (
 								<span
 									className={`block px-2 truncate w-full ${
