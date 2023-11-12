30797f4a0d9b62800fdbc4535a82d20a23ee0f8c
RadBear <nbhung71711@gmail.com>
2023-11-13 05:34:55 +0700

    1

--- lib/reviewsStatic.tsx
+++ lib/reviewsStatic.tsx
@@ -4,7 +4,10 @@ import { marked } from "marked";
 import searchData from "./searchData";
 
 export async function getReview(name: string) {
-	const text = await readFile(process.cwd() + `/content/${name}.md`, "utf-8");
+	const text = await readFile(
+		process.cwd() + `/public/content/${name}.md`,
+		"utf-8"
+	);
 	const {
 		content,
 		data: { slug, title, subtitle, image, publishedAt },
@@ -41,7 +44,7 @@ export async function getReviews(pageSize, page = 1) {
 }
 
 export async function getSlugs() {
-	const files = await readdir(process.cwd() + "/content/");
+	const files = await readdir(process.cwd() + "/public/content/");
 	const slugs = files
 		.filter((f) => f.endsWith(".md"))
 		.map((f) => f.slice(0, -".md".length));
