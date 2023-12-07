fda1c21267c721cb8582a2e4f5fff1dae6dc1930
RadBear <nbhung71711@gmail.com>
2023-12-07 21:53:52 +0700

    Displaying Action Errors

--- app/components/CommentForm.tsx
+++ app/components/CommentForm.tsx
@@ -1,10 +1,31 @@
 'use client'
+import { useState } from 'react'
 import { submitComment } from '../reviews/[slug]/action'
 
 function CommentForm({ slug, title }) {
+  const [state, setState] = useState({ loading: false, error: null })
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    setState({ loading: true, error: null })
+    const form: HTMLFormElement = event.currentTarget
+    const formData = new FormData(form)
+    const user = formData.get('user') as string
+    const message = formData.get('message') as string
+    // console.log('fromData', Array.from(formData.entries()))
+    const result = await submitComment({ slug, user, message })
+    if (result?.isError) {
+      setState({ loading: false, error: result.message })
+    } else {
+      form.reset()
+      setState({ loading: false, error: null })
+    }
+    return result
+  }
+
   return (
     <form
-      action={submitComment}
+      onSubmit={handleSubmit}
       className="mt-3 flex flex-col gap-2 rounded border bg-white px-3 py-2"
     >
       <p className="pb-1">
@@ -36,8 +57,10 @@ function CommentForm({ slug, title }) {
           className="w-full rounded border px-2 py-1"
         />
       </div>
+      {state.error && <p className="text-red-500">{state.error}</p>}
       <button
         type="submit"
+        disabled={state.loading}
         className="w-32 self-center rounded bg-orange-800 px-2 py-1 text-white hover:bg-orange-700 disabled:cursor-not-allowed disabled:bg-slate-500"
       >
         Submit
--- app/reviews/[slug]/action.tsx
+++ app/reviews/[slug]/action.tsx
@@ -3,17 +3,42 @@ import { createComment } from '@/lib/comments'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
-export async function submitComment(formData) {
-  if (!formData.get('user') || !formData.get('message')) {
-    return { isError: true, message: 'Please enter your name and comment' }
-  }
+export async function submitComment({
+  slug,
+  user,
+  message,
+}: {
+  slug: string
+  user: string
+  message: string
+}) {
   const data = {
-    slug: formData.get('slug'),
-    user: formData.get('user'),
-    message: formData.get('message'),
+    slug: slug,
+    user: user,
+    message: message,
   }
-  const comment = await createComment(data)
+  const error = validate(data)
+  if (error) {
+    return { isError: true, message: error }
+  }
+
+  await createComment(data)
   const url = `/reviews/${data.slug}`
   revalidatePath(url)
   redirect(url)
+}
+
+function validate(data): string | undefined {
+  if (!data.user) {
+    return 'Name field is required'
+  }
+  if (data.user.length > 50) {
+    return 'Name field cannot be longer than 50 characters'
+  }
+  if (!data.message) {
+    return 'Comment field is required'
+  }
+  if (data.message.length > 500) {
+    return 'Comment field cannot be longer than 500 characters'
+  }
 }
